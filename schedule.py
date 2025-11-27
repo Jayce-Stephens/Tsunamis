d054e99794498ca5179e41529bd64a133ad4d029
Jayce <jstephe3@xula.edu>
2025-11-27 01:53:30 -0500

    Add viewSchedule function Add prefilled_students_map

--- schedule.py
+++ schedule.py
@@ -8,15 +8,42 @@ class Schedule:
     def addCourse(self, course):
         self.courses.append(course)
 
+    def viewSchedule(self):
+        print("Student: " + self.student_name)
+
+        for c in self.courses:
+            print("-" + c)
+
+
     
 
 def main():
-    s = Schedule("Austin")
-    s.addCourse("Math")
-    s.addCourse("Comp Sci")
+    prefilled_student_map = {
+        "Austin": [
+            ("MATH 1090"), ("CPSC 3029"), ("ENGL 9903"), ("CPSC 1210")
+        ],
+        "Drake": [
+            ("MATH 3390"), ("CPSC 1590"), ("ENGL 9903"), ("MATH 1101")
+        ],
+        "Sarah": [
+            ("CPSC 3030"), ("HIST 2203"), ("XCOR 9993"), ("CPSC 4403")
+        ],
+        "Tony": [
+            ("CPSC 1590"), ("CPSC 3029"), ("ENGL 9903"), ("MATH 3390")
+        ]
+    }
 
-    for c in s.courses:
-        print(c)
+    students = {}
+
+    for name, courses in prefilled_student_map.items():
+        s = Schedule(name)
+        for c in courses:
+            s.addCourse(c)
+
+        students[name] = s
+    
+    for name in students:
+        students[name].viewSchedule()
     
 
 if __name__ == "__main__":
