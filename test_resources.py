91ecc546e036f915d47e47801df84e9ce3d922b2
Jayce <jstephe3@xula.edu>
2025-12-01 16:20:21 -0600

    Create test to ensure that load_default_resources returns a list of resources.

--- test_resources.py
+++ test_resources.py
@@ -1,1 +1,12 @@
 import unittest
+from resources import Resource, load_default_resources
+
+class TestResources(unittest.TestCase):
+
+    def test_returns_list(self):
+        resources = load_default_resources()
+
+        self.assertIsInstance(resources, list)
+
+
+
